c08d7aec015be547f509407d66d58a00afb9310a
Clement Marchand <clement@studioberenice.com>
2026-03-21 12:14:23 +0100

    Add lheureuxtour

--- components/AdBanner970.tsx
+++ components/AdBanner970.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function AdBanner970() {
     return (
         <section className="ad-slot">
             <div className="ad-slot-inner">
-                Emplacement publicité 970 × 250
+                {/* Emplacement publicité 970 × 250 */}
+                <img
+                    src="/images/lheureuxtour.jpg"
+                    alt="Bannière"
+                    width={970}
+                    height={250}
+                    style={{ width: "100%", height: "auto" }}
+                />
             </div>
         </section>
     );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,36 @@ export default function Footer() {
     return (
         <footer className="site-footer">
             <div className="social-row">
-                <a href="#">Facebook</a>
-                <a href="#">Instagram</a>
-                <a href="#">Spotify</a>
-                <a href="#">Deezer</a>
-                <a href="#">Amazon Music</a>
-                <a href="#">YouTube</a>
+                <a href="https://www.facebook.com/lachansondudimanche" target="_blank"><img
+                    src="/images/facebook.png"
+                    alt="Facebook de La Chanson du Dimanche"
+                    className="footer-image"
+                /></a>
+                <a href="https://www.instagram.com/lachansondudimanche" target="_blank"><img
+                    src="/images/instagram.png"
+                    alt="Instagram de La Chanson du Dimanche"
+                    className="footer-image"
+                /></a>
+                <a href="https://open.spotify.com/artist/6O4fmo4MKyR4bfBeUjqLdX" target="_blank"><img
+                    src="/images/spotify.png"
+                    alt="Spotify de La Chanson du Dimanche"
+                    className="footer-image"
+                /></a>
+                <a href="https://www.deezer.com/fr/artist/17258" target="_blank"><img
+                    src="/images/deezer.png"
+                    alt="Deezer de La Chanson du Dimanche"
+                    className="footer-image"
+                /></a>
+                <a href="https://music.amazon.fr/artists/B0025SBUJA/la-chanson-du-dimanche" target="_blank"><img
+                    src="/images/amazon.png"
+                    alt="Amazon de La Chanson du Dimanche"
+                    className="footer-image"
+                /></a>
+                <a href="https://www.youtube.com/@lachansondudimanche" target="_blank"><img
+                    src="/images/youtube.png"
+                    alt="YouTube de La Chanson du Dimanche"
+                    className="footer-image"
+                /></a>
             </div>
         </footer>
     );
