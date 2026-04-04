1ac7cea07c89e93507237b9b25c1efa524db01aa
Clement Marchand <clement@studioberenice.com>
2026-04-04 18:06:44 +0200

    Add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -7,6 +8,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XK3P09FBYJ"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XK3P09FBYJ');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
